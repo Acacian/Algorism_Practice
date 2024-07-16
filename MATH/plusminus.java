323f4df2bb918b2e4522f7bb50bdc373c921f70e
DOHA <koo9811@gmail.com>
2024-07-16 16:26:53 +0900

    java basic

--- MATH/plusminus.java
+++ MATH/plusminus.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class devide{
+public class plusminus{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         float a = in.nextFloat();
         float b = in.nextFloat();
-        System.out.println(a / b);
+        System.out.println(a + b);
         in.close();
     }
 }
