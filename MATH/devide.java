78c19b766b14699312e97d5c4511e2ad40359e81
DOHA <koo9811@gmail.com>
2024-07-16 16:17:06 +0900

    java basic

---
+++ MATH/devide.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class devide{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        float a = in.nextInt();
+        float b = in.nextInt();
+        System.out.println(a / b);
+        in.close();
+    }
+}
