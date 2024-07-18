e24c698005c0bcbd5965818de843941ef2cabd42
DOHA <koo9811@gmail.com>
2024-07-18 14:49:26 +0900

    daily init

---
+++ ARRAYANDLIST/s2003.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class s2003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+        int NM = st.countTokens();
+        int li = st.countTokens();
+        int[] lis = new int[li];
+    }
+}
