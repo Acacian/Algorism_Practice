a387cb9f9efea599b3feb689155e62ffe83d5158
DOHA <koo9811@gmail.com>
2024-07-19 20:53:55 +0900

    daily init

--- RECURSION/4779.py
+++ RECURSION/4779.py
@@ -6,12 +6,21 @@ T = int(input())
 def kantore(x):
     # 예외처리조건
     if x == 0:
-        return 0
+        return "-"
 
     # 작대기 길이
     row = "-" * (3 ** x)
-
-
+    if x > 0:
+        # 이전 단계의 칸토어 집합 생성
+        prev = kantore(x - 1)
+        
+        # 새로운 칸토어 집합 생성
+        left = prev
+        middle = " " * len(prev)
+        right = prev
+        
+        # 결과 조합
+        row = left + middle + right
 
     return row
         
