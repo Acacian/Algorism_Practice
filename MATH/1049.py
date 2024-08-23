5ff904ddbde82f1172289f94b7b580666db77607
DOHA <koo9811@gmail.com>
2024-08-23 22:18:49 +0900

    daily init

---
+++ MATH/1049.py
@@ -0,0 +1,27 @@
+# 이거 모든 예제 + 질문게시판에 있는 반례 통과하는데 틀렸다고 하는 이유를 모르겠음
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+li = []
+six,one = 1e9,1e9
+for _ in range(M):
+    s,o = map(int,input().split())
+    if o < one:
+        one = o
+    if s < six:
+        if s > one * 6:
+            six = one * 6
+        else:
+            six = s
+
+# 최소값.. 
+ans = 0
+ans += (N // 6) * six
+
+if (N % 6) * one > six:
+    ans += six
+else:
+    ans += one * (N % 6)
+
+print(int(ans))
