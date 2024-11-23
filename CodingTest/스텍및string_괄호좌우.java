56cb9bfd49a8fdfaeb91eaca879bfb379327aa1e
DOHA <koo9811@gmail.com>
2024-11-23 18:57:41 +0900

    공부중

--- CodingTest/스텍및string_괄호좌우.java
+++ CodingTest/스텍및string_괄호좌우.java
@@ -1,8 +1,6 @@
 import java.util.*;  
   
-class Solution {  
-  
-    boolean solution(String s) {  
+class Solution {boolean solution(String s) {  
         Stack<Character> stack = new Stack<>();  
         for (int i = 0; i < s.length(); i++) {  
             if (s.charAt(i) == '(') {  
---
+++ CodingTest/큐및int_프로세스.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> queue = new LinkedList<>(); 
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[] { priorities[i], i }); 
+        }
+        int order = 0;
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            if (queue.stream().anyMatch(p -> p[0] > current[0])) {
+                queue.add(current); 
+            } else {
+                order++; 
+                if (current[1] == location) {
+                    return order;
+                }
+            }
+        }
+        return order;
+    }
+}
